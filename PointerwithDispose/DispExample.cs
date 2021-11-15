d35bc25bf6c58fc7e01758d733317a1ef6dc0ae1
dnyana <dnyaneshgawade903@gmail.com>
2021-11-15 09:51:41 +0530

    [Dnyanesh] Add : performing Dispose and Finalize Methods

---
+++ PointerwithDispose/DispExample.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PointerwithDispose
+{
+    class DispExample : IDisposable
+    {
+        private bool disposed = false;
+        public void Greeting(string name)
+        {
+            Console.WriteLine(string.Format("Hello {0} ", name));
+        }
+        ~DispExample()
+        {
+            Console.WriteLine("Inside Finalize");
+            Dispose(false);
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+        protected void Dispose(bool disposing)
+        {
+            if (!disposed)
+            {
+                if (disposing)
+                {
+                    Console.WriteLine("Cleanup Managed Object");
+                }
+                Console.WriteLine("Cleanup unmanaged Object");
+                disposed = true;
+            }
+        }
+    }
+}
---
+++ PointerwithDispose/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace PointerwithDispose
+{
+    struct MyStruct
+    {
+        public int x;
+        public int y;
+        public void SetXY(int i, int j)
+        {
+            x = i;
+            y = j;
+        }
+        public void ShowXY()
+        {
+            Console.WriteLine(x);
+            Console.WriteLine(y);
+        }
+    }
+    class MyClient
+    {
+        public unsafe static void Main()
+        {
+
+            MyStruct ms = new MyStruct();
+            MyStruct* ms1 = &ms;
+            ms1->SetXY(10, 20);
+            ms1->ShowXY();
+            DispExample dsp = new DispExample();
+            Console.WriteLine("DispExample Object created...");
+            dsp.Greeting("Dnyanesh");
+            dsp.Dispose();
+            dsp = null;
+            Console.WriteLine("Assigned Null... Object Distructing...");
+            Console.ReadLine();
+
+        }
+    }
+}
